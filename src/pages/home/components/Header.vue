9169568f9b64866a6c358272b0fea8228746b472
leahzheng <zan272905918@163.com>
2020-10-14 16:05:02 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <span class="iconfont">&#xe624;</span>
+        </div>
+        <div class="header-middle">
+            <span class="iconfont search-icon">&#xe632;</span>
+            <div class="kw">搜索景点门票</div>
+            <input type="text" class="header-input">
+        </div>
+        <div class="header-right">
+            城市
+        <span class="iconfont">&#xe6aa;</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl';
+.header{
+    display: flex;
+    width: 100%;
+    height: 0.88rem;
+    color: #fff;
+    background: $bgColor;
+}
+.header .header-left{
+    width: .8rem;
+    height: 100%;
+    line-height: .88rem;
+    text-align: center;
+}
+.header .header-middle{
+    position: relative;
+    flex:1;
+    margin: .14rem 0;
+}
+.header .header-middle .search-icon{
+    position:absolute;
+    top: .13rem;
+    left: .2rem;
+    color: #e4e7ea;
+}
+
+.header .header-middle .kw{
+    position: absolute;
+    top: .18rem;
+    left: .65rem;
+    color: #e4e7ea;
+}
+
+.header .header-middle .header-input{
+    height: 100%;
+    width: 100%;
+    border-radius: .05rem;
+}
+
+.header .header-right{
+    width:1.32rem;
+    height: 100%;
+    line-height: .88rem;
+    text-align: center;
+}
+</style> 
